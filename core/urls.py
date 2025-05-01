d0bd03accac10702655b48bac14da01c81d4418e
Aynal Haque <aynalhaque7411@gmail.com>
2025-05-01 13:35:35 +0000

    six

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import home_page
+from .views import home_page, projects
 urlpatterns = [
     path("", home_page, name="home"),
+     path('projects/', projects, name='projects'),
 ]
--- core/views.py
+++ core/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'home.html')
+
+def projects(request):
+    return render(request, 'projects.html') 
